d67c01523b970d2531b2c5aff99a0d70af36bc36
priscila monteiro <pririmonteiro@me.com>
2021-12-21 15:59:42 -0800

    wip

--- api/classes/classes-middleware.js
+++ api/classes/classes-middleware.js
@@ -1,84 +1,95 @@
-const Classes = require('./classes-model')
-const db = require('../data/db-config')
+const Classes = require("./classes-model");
+const db = require("../data/db-config");
 
 exports.checkAccountPayload = (req, res, next) => {
-  const error = { status: 400 }
-  const { name, type, start_time, intensity_level, location, number_registered, max_class_size } = req.body
-  if (name === undefined || start_time === undefined || intensity_level === undefined) {
-    error.message = 'name start_time intensity_level are required'
-    next(error)
+  const error = { status: 400 };
+  const {
+    class_name,
+    class_type,
+    start_time,
+    class_intensity_level,
+    class_location,
+    // number_registered,
+    max_attendees,
+  } = req.body;
+  if (
+    class_name === undefined ||
+    start_time === undefined ||
+    class_intensity_level === undefined
+  ) {
+    error.message = "name start_time intensity_level are required";
+    next(error);
   }
-  if (type === undefined || location === undefined || number_registered === undefined || max_class_size === undefined) {
-    error.message = 'type location number_registred and max_class_size are required'
-    next(error)
-  }
-  else if (typeof name !== 'string') {
-    error.message = 'name of classes must be a string'
-    next(error)
-  }
-  else if (name.trim().length < 3 || name.trim().length > 100) {
-    error.message = 'name of classes must be between 3 and 100'
-    next(error)
-  }
-  else if (typeof max_class_size !== 'number' || isNaN(max_class_size)) {
-    error.message = 'message: max_class_size of classes must be a number'
-    next(error)
-  }
-  else if (typeof number_registered !== 'number' || isNaN(number_registered)) {
-    error.message = 'message: number_registered of classes must be a number'
-    next(error)
-  }
-  else if (typeof type !== 'string') {
-    error.message = 'type of classes must be a string'
-    next(error)
-  }
-  else if (typeof start_time !== 'string') {
-    error.message = 'start_time of classes must be a string'
-    next(error)
-  }
-  else if (typeof intensity_level !== 'string') {
-    error.message = 'intensity_level of classes must be a string'
-    next(error)
-  }
-  else if (typeof location !== 'string') {
-    error.message = 'location of classes must be a string'
-    next(error)
+  if (
+    class_type === undefined ||
+    class_location === undefined ||
+    // number_registered === undefined ||
+    max_attendees === undefined
+  ) {
+    error.message =
+      "type location number_registered and max_class_size are required";
+    next(error);
+  } else if (typeof class_name !== "string") {
+    error.message = "name of classes must be a string";
+    next(error);
+  } else if (class_name.trim().length < 3 || class_name.trim().length > 100) {
+    error.message = "name of classes must be between 3 and 100";
+    next(error);
+  } else if (typeof max_attendees !== "number" || isNaN(max_attendees)) {
+    error.message = "message: max_attendees of classes must be a number";
+    next(error);
+    // } else if (
+    //   typeof number_registered !== "number" ||
+    //   isNaN(number_registered)
+    // ) {
+    //   error.message = "message: number_registered of classes must be a number";
+    //   next(error);
+  } else if (typeof class_type !== "string") {
+    error.message = "type of classes must be a string";
+    next(error);
+  } else if (typeof start_time !== "string") {
+    error.message = "start_time of classes must be a string";
+    next(error);
+  } else if (typeof class_intensity_level !== "string") {
+    error.message = "intensity_level of classes must be a string";
+    next(error);
+  } else if (typeof class_location !== "string") {
+    error.message = "location of classes must be a string";
+    next(error);
   }
   if (error.message) {
-    next(error)
+    next(error);
   } else {
-    next()
+    next();
   }
-}
+};
 
-  exports.checkAccountNameUnique = async (req, res, next) => {
-    try {
-      const existing = await db('classes')
-        .where('name', req.body.name.trim())
-        .first()
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try {
+    const existing = await db("classes")
+      .where("name", req.body.name.trim())
+      .first();
 
-      if (existing) {
-        next({ status: 400, message: 'that name is taken' })
-      } else {
-        next()
-      }
-    } catch (err) {
-      next(err)
+    if (existing) {
+      next({ status: 400, message: "that name is taken" });
+    } else {
+      next();
     }
+  } catch (err) {
+    next(err);
   }
+};
 
-  exports.checkAccountId = async (req, res, next) => {
-
-    try {
-      const classes = await Classes.getById(req.params.id)
-      if (!classes) {
-        next({ status: 404, message: 'classes not found' })
-      } else {
-        req.classes = classes
-        next()
-      }
-    } catch (err) {
-      next(err)
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const classes = await Classes.getById(req.params.id);
+    if (!classes) {
+      next({ status: 404, message: "classes not found" });
+    } else {
+      req.classes = classes;
+      next();
     }
+  } catch (err) {
+    next(err);
   }
-
+};
--- api/classes/classes-router.js
+++ api/classes/classes-router.js
@@ -1,71 +1,74 @@
-const router = require('express').Router()
-const { checkAccountId, checkAccountNameUnique, checkAccountPayload } = require('./classes-middleware')
-const { restricted } = require('../auth/auth-middleware')
-const Classes = require('./classes-model')
+const router = require("express").Router();
+const {
+  checkAccountId,
+  checkAccountNameUnique,
+  checkAccountPayload,
+} = require("./classes-middleware");
+const { restricted } = require("../auth/auth-middleware");
+const Classes = require("./classes-model");
 
-router.get('/', restricted, async (req, res, next) => {
+router.get("/", restricted, async (req, res, next) => {
   try {
-    const classes = await Classes.getAll()
-    res.status(200).json(classes)
+    const classes = await Classes.getAll();
+    res.status(200).json(classes);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-router.get('/:id', restricted, checkAccountId , async (req, res, next) => {
-  try{
-    res.json(req.classes)
+router.get("/:id", restricted, checkAccountId, async (req, res, next) => {
+  try {
+    res.json(req.classes);
+  } catch (err) {
+    next(err);
   }
-  catch(err){
-    next(err)
-  }
-})
+});
 
 router.post(
-  '/',
+  "/",
   restricted,
   checkAccountPayload,
   checkAccountNameUnique,
   async (req, res, next) => {
     try {
       const newClasses = await Classes.create({
-        name: req.body.name.trim(),
-        type: req.body.type,
+        class_name: req.body.class_name.trim(),
+        class_type: req.body.class_type,
         start_time: req.body.start_time,
-        intensity_level: req.body.intensity_level,
-        location: req.body.location,
-        number_registered: req.body.number_registeted,
-        max_class_size: req.body.max_class_size,
-
-
-      })
-      res.status(201).json(newClasses)
+        class_intensity_level: req.body.class_intensity_level,
+        class_location: req.body.class_location,
+        // number_registered: req.body.number_registered,
+        max_attendees: req.body.max_attendees,
+      });
+      res.status(201).json(newClasses);
     } catch (err) {
-      next(err)
+      next(err);
     }
-  })
+  }
+);
 
 router.put(
-  '/:id',
+  "/:id",
   restricted,
   checkAccountId,
   checkAccountPayload,
   async (req, res, next) => {
     try {
-      const updated = await Classes.updateById(req.params.id, req.body)
-      res.json(updated)
+      const updated = await Classes.updateById(req.params.id, req.body);
+      res.json(updated);
     } catch (err) {
-      next(err)
+      next(err);
     }
-  });
-
-router.delete('/:id', restricted, checkAccountId, async (req, res, next) => {
-  try {
-    await Classes.deleteById(req.params.id)
-    res.json(req.classes)
-  } catch (err) {
-    next(err)
   }
-})
+);
+
+router.delete("/:id", restricted, checkAccountId, async (req, res, next) => {
+  try {
+    await Classes.deleteById(req.params.id);
+    res.json(req.classes);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
